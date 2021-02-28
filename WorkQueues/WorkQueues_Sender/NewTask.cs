6a13b7ea4f5f20176b7754c74b9868a05cce775f
FreddyFox1 <svchaykun@mail.ru>
2021-02-28 20:36:11 +0300

    Add  WorkQueues_Sender

---
+++ WorkQueues/WorkQueues_Sender/NewTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace WorkQueues_Task
+{
+    public class NewTask
+    {
+        public void Sender()
+        {
+            //Задаем имя хоста брокера сообщений
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            //Создаем соединение с хостом
+            var connection = factory.CreateConnection();
+            //Создаем канал для связи
+            var channel = connection.CreateModel();
+            //Объявляем очередь с именем "task_queue", 
+            //durable = true(в случае ошибки очередь будет сохранена)
+            channel.QueueDeclare("task_queue", true, false,
+                false, null);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            //Отправляем сообщение в очередь с именем "task_queue"
+            channel.BasicPublish("", "task_queue", properties, body);
+            Console.WriteLine(" [x] Отправлено {0}", message);
+
+            Console.WriteLine(" Нажмите [enter] для выхода.");
+            Console.ReadLine();
+
+            //Закрываем соединения
+            connection.Close();
+            channel.Close();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Length > 0 ? string.Join(" ", args) : "Hello World!";
+        }
+    }
+}
---
+++ WorkQueues/WorkQueues_Sender/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkQueues_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewTask NewTask = new NewTask();
+            NewTask.Sender();
+        }
+    }
+}
